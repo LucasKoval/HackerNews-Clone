a24cba858523f79a36fbf8dc1ef0ef56667689b1
LucasKoval <lucaskoval@hotmail.com>
2021-03-19 17:15:49 -0300

    Modified style details

--- src/hooks/dataFetcher.js
+++ src/hooks/dataFetcher.js
@@ -4,6 +4,8 @@ import { getStories } from '../utils/apis';
 const useDataFetcher = (type) => {
   const [stories, setStories] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+/*   const [currentPage, setCurrentPage] = useState(1);
+  const [storyPerPage, setStoryPerPage] = useState(10); */
 
   useEffect(() => {
     setIsLoading(true);
@@ -16,6 +18,10 @@ const useDataFetcher = (type) => {
         setIsLoading(false);
       });
   }, [type]);
+
+/*   const indexOfLastStory = currentPage * storyPerPage;
+  const indexOfFirstStory = indexOfLastStory - storyPerPage;
+  const currentStories = stories.slice(indexOfFirstStory, indexOfLastStory); */
 
   return { isLoading, stories };
 };
